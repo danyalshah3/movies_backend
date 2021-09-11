7e6de77707e8710c311880b3798b7158d3ebb0fc
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-09-11 14:41:41 -0400

    added create method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,15 @@ class MoviesController < ApplicationController
         movies = Movie.all
         render json: movies
     end
+
+    def create
+        movie = Movie.create(movie_params)
+        render json: movie
+    end
+
+    private
+
+    def movie_params
+   params.require(:movie).permit(:title, :summary, :duration, :image)
+    end
 end
