7b07fb57de236fcb56d6fd6328e2506f6cb24f90
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-09-18 15:36:11 -0400

    migrated user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_181507) do
+ActiveRecord::Schema.define(version: 2021_09_18_193435) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2021_09_12_181507) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "reviews", "movies"
