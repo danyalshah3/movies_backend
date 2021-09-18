d27b0bbbaa50750cec1816a33704336e274ac982
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-09-18 15:44:07 -0400

    added user to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_193435) do
+ActiveRecord::Schema.define(version: 2021_09_18_194123) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -27,7 +27,9 @@ ActiveRecord::Schema.define(version: 2021_09_18_193435) do
     t.integer "movie_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_09_18_193435) do
   end
 
   add_foreign_key "reviews", "movies"
+  add_foreign_key "reviews", "users"
 end
